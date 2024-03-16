2d789b79f35dcc9350a7ab9ef8763134cff9383f
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2024-03-16 20:00:41 +0100

    Update getMovies.ts

--- app/lib/getMovies.ts
+++ app/lib/getMovies.ts
@@ -1,7 +1,7 @@
 'use server'
 const { API_KEY} = process.env
 
-export const getMovies = async (page='1', genre='false', year='false') => {
+export const getMovies = async (page='1', genre='', year='') => {
     const resp = await fetch(`https://api.themoviedb.org/3/discover/movie?page=${page}&primary_release_year=${year}&with_genres=${genre}&api_key=${API_KEY}`);
     if (!resp.ok) {
     throw new Error('Failed to fetch data')
