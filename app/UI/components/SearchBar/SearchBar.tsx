aad295725509b8600ec680c2ee13cea4db7f2b05
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2024-03-28 23:01:28 +0100

    Update SearchBar.tsx

--- app/UI/components/SearchBar/SearchBar.tsx
+++ app/UI/components/SearchBar/SearchBar.tsx
@@ -1,15 +1,21 @@
 'use client'
-import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { MdOutlineRadioButtonChecked } from "react-icons/md";
 import { MdRadioButtonUnchecked } from "react-icons/md";
 
 export default function SearchBar() {
     const params = useSearchParams()
+    const pathname = usePathname()
     const [query, setQuery] = useState(params.get('query') || '')
     const [isMovieChecked, setIsMovieChecked] = useState(true)
     const router = useRouter()
+
+    useEffect(() => {
+        setIsMovieChecked(!pathname.includes('/tv'))
+    }, [pathname])
+
     const handlerSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault()
         if (!query) return
@@ -20,24 +26,26 @@ export default function SearchBar() {
         router.push(`/tvsearch?query=${query}`)
     }
 
-    return (
+    return (<div className="relative max-w-[450px] w-full">
         <form className="flex gap-3 items-center" onSubmit={handlerSubmit}>
-            <div className="relative text-[16px]">
-                <input className="bg-transparent border-2 border-neutral-400 rounded-full outline-none focus:border-neutral-300 appearance-none px-3 py-1 transition-all" value={query} type="text" onChange={(e) => setQuery(e.target.value)} />
+            <div className="relative text-[16px] w-full">
+                <input className="w-full bg-transparent border-2 border-neutral-400 rounded-full outline-none focus:border-neutral-300 appearance-none px-3 py-1 transition-all" value={query} type="text" onChange={(e) => setQuery(e.target.value)} />
                 <button className="absolute right-3 top-1/2 translate-y-[-50%] group text-xl"><IoIosSearch className="group-hover:fill-neutral-200 transition-colors" /></button>
             </div>
             <div className=" text-[16px] flex gap-2">
                 <div className="flex items-center gap-1">
                     <label className="cursor-pointer" htmlFor="movies">{isMovieChecked ? <MdOutlineRadioButtonChecked /> : <MdRadioButtonUnchecked />}</label>
-                    <input type="radio" name="search" id="movies" hidden defaultChecked onChange={() => setIsMovieChecked(!isMovieChecked)} />
+                    <input type="radio" name="search" id="movies" hidden checked={isMovieChecked} onChange={() => setIsMovieChecked(true)} />
                     <label className="cursor-pointer" htmlFor="movies">Movie</label>
                 </div>
                 <div className="flex items-center gap-1">
                     <label className="cursor-pointer" htmlFor="tvs">{!isMovieChecked ? <MdOutlineRadioButtonChecked /> : <MdRadioButtonUnchecked />}</label>
-                    <input type="radio" name="search" id="tvs" hidden onChange={() => setIsMovieChecked(!isMovieChecked)} />
+                    <input type="radio" name="search" id="tvs" checked={!isMovieChecked} hidden onChange={() => setIsMovieChecked(false)} />
                     <label className="cursor-pointer" htmlFor="tvs" >TV</label>
                 </div>
             </div>
         </form>
+        <div className="absolute top-[120%] w-full bg-neutral-700 z-10 rounded-md p-2"></div>
+    </div>
     )
 }
